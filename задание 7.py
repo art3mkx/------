e8d4cc97ee854f9ba644646501abe8ae34fbd3e2
art3mkx <artemackevich@gmail.com>
2025-06-21 17:57:48 +0300

    Create задание 7.py

---
+++ задание 7.py
@@ -0,0 +1,18 @@
+def group_anagrams(words):
+    if not words:
+        return []
+
+    anagram_groups = {}
+    
+    for word in words:
+        normalized = ''.join(sorted(word.lower()))
+        
+        if normalized not in anagram_groups:
+            anagram_groups[normalized] = []
+        anagram_groups[normalized].append(word)
+    
+    return list(anagram_groups.values())
+
+input_words = ['cars', 'for', 'potatoes', 'racs', 'four', 'scar', 'creams', 'scream']
+result = group_anagrams(input_words)
+print(result)
