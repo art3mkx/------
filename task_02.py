92273ba212bb2233beecf76ab4341bfa30584dc9
art3mkx <artemackevich@gmail.com>
2025-06-23 15:25:01 +0300

    Update task_02.py

--- task_02.py
+++ task_02.py
@@ -5,12 +5,10 @@ def coincidence(lst=None, rng=None):
     result = []
     start = rng.start
     stop = rng.stop
-    step = rng.step if rng.step is not None else 1
     
     for item in lst:
         if isinstance(item, (int, float)):
             if start <= item < stop:
-                if abs((item - start) % step) < 1e-10:
-                    result.append(item)
+                result.append(item)
     
     return result
