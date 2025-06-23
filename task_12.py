fb35cff8ec74f08796a46d75ae7fb620297473bd
art3mkx <artemackevich@gmail.com>
2025-06-23 13:03:53 +0300

    task_12.py

--- task_12.py
+++ task_12.py
@@ -1,3 +1,31 @@
+class Dessert:
+    def __init__(self, name=None, calories=None):
+        self._name = name
+        self._calories = calories
+    
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, value):
+        self._name = value
+    
+    @property
+    def calories(self):
+        return self._calories
+    
+    @calories.setter
+    def calories(self, value):
+        self._calories = value
+    
+    def is_healthy(self):
+        return self._calories is not None and self._calories < 200
+    
+    def is_delicious(self):
+        return True
+
+
 class JellyBean(Dessert):
     def __init__(self, name=None, calories=None, flavor=None):
         super().__init__(name, calories)
@@ -12,6 +40,4 @@ class JellyBean(Dessert):
         self._flavor = value
     
     def is_delicious(self):
-        if self._flavor == "black licorice":
-            return False
-        return True
+        return self._flavor != "black licorice"
